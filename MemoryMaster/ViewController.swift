f95bf934dbf1a0ea8d31eaca09a054e3979619b1
hhsolar <hongh17717@gmail.com>
2017-10-01 16:59:13 -0700

    first commit

---
+++ MemoryMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryMaster
+//
+//  Created by apple on 1/10/2017.
+//  Copyright © 2017 greatwall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
